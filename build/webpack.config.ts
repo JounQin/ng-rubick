025e22b121b240b5d0407bd37ec2cd28adee8a4b
Jiaren Liu <jrliu@alauda.io>
2018-04-19 10:52:33 +0800

    no need to cast any more

--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -118,7 +118,7 @@ const config: Configuration = {
       compilerOptions: {
         disableTypeScriptVersionCheck: true,
       },
-    } as any),
+    }),
     new MiniCssExtractPlugin({
       filename: `[name].[${hashType}].css`,
     }),
