b3935fdd3bbe4ff31684ab69b1df11f16d12741e
Jiaren Liu <jrliu@alauda.io>
2018-06-03 13:37:54 +0800

    fix: upgrade koa-webpack

--- server/dev.ts
+++ server/dev.ts
@@ -1,6 +1,5 @@
 import koaWebpack from 'koa-webpack'
-import webpack from 'webpack'
 
 import config from '../build/webpack.config'
 
-export default koaWebpack({ compiler: webpack(config) })
+export default koaWebpack({ config })
